2ec89f2dc2a5230b61b9637b2137e882d928d4a3
Panhchna04 <mpanhchna@gmail.com>
2024-01-14 23:44:12 +0700

    first commit

---
+++ src/Exercise1.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+    public class Exercise1 {
+        public static void main(String[] args) {
+            Scanner cin = new Scanner(System.in);
+            System.out.println("Enter a short date (YYYY-MM-DD): ");
+            String shortDate=cin.nextLine();
+            int year = Integer.parseInt(shortDate.substring(0, 4));
+            int month = Integer.parseInt(shortDate.substring(5, 7));
+            int day = Integer.parseInt(shortDate.substring(8));
+            String[] monthNames = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+            String monthName = monthNames[month - 1];
+            System.out.println(monthName + " " + day + ", " + year);
+        }
+
+    }
+
